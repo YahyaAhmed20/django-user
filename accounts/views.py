80532f63aa1c720c4b5c73e00ba3adb77db4ad1f
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-01-27 04:47:27 +0200

    Added gmail

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,16 @@
+from django.core.mail import send_mail
+from django.conf import settings  # Import settings to access DEFAULT_FROM_EMAIL
 from django.contrib.auth import views as auth_views
 from .forms import EmailOrUsernameLoginForm
 from urllib import request
 from django.shortcuts import redirect, render
-from .forms import SignupForm,UserForm,ProfileForm
-from django.contrib.auth import authenticate,login
+from .forms import SignupForm, UserForm, ProfileForm
+from django.contrib.auth import authenticate, login
 from .models import Profile
 from django.urls import reverse
-
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 def signup(request):
     if request.method == 'POST':
@@ -15,14 +19,27 @@ def signup(request):
             user = form.save()  # Save the user and get the user instance
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
+            email = form.cleaned_data['email']  # Define 'email' from the form's cleaned data
             user = authenticate(request, username=username, password=password)
 
             if user is not None:
                 login(request, user)
+
+                # Send welcome email
+                subject = 'Welcome to Our Website!'
+                message = f'Hello {username}, thank you for signing up!'
+                send_mail(
+                    subject,
+                    message,
+                    settings.DEFAULT_FROM_EMAIL,  # Use DEFAULT_FROM_EMAIL from settings
+                    [email],  # Use the 'email' variable defined above
+                    fail_silently=False,
+                )
+
                 # Check if the user already has a profile
                 if not hasattr(user, 'profile'):
                     Profile.objects.create(user=user)  # Create a profile only if it doesn't exist
-                return redirect('/accounts/profile')
+                    return redirect('/accounts/profile')
     else:
         form = SignupForm()
 
--- project/settings.py
+++ project/settings.py
@@ -129,3 +129,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER ='yahmdh6@gmail.com'
+EMAIL_HOST_PASSWORD='nzmcfxgowwjxlofn'
+DEFAULT_FROM_EMAIL = 'yahmdh6@gmail.com'  # Default sender email
+
+EMAIL_USE_TLS =True
+EMAIL_PORT= 587
